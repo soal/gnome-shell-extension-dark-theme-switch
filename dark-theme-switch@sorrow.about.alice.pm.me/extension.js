0fcfa413d1fef2882f565d90eb9e55aa8959b994
soal <sorrow.about.alice@pm.me>
2019-05-01 18:53:13 +0300

    Added license to extension code

--- dark-theme-switch@sorrow.about.alice.pm.me/extension.js
+++ dark-theme-switch@sorrow.about.alice.pm.me/extension.js
@@ -1,3 +1,21 @@
+/*
+  Dark Theme Switch Gnome Shell Extension
+  Developed by Aleksey Pozharov sorrow.about.alice@pm.me
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see
+  < https://www.gnu.org/licenses/gpl-2.0.html >.
+  This program is a derived work of Gnome Shell.
+*/
+
 const Main = imports.ui.main;
 const Gio = imports.gi.Gio;
 const PopupMenu = imports.ui.popupMenu;
